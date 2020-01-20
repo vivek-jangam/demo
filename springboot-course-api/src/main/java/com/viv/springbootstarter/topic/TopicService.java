f9df4728a5f2464736beea6d98513ad33f0afa63
vivek-jangam <60109507+vivek-jangam@users.noreply.github.com>
2020-01-20 23:11:53 +0530

    First commit demo

---
+++ springboot-course-api/src/main/java/com/viv/springbootstarter/topic/TopicService.java
@@ -0,0 +1,47 @@
+package com.viv.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("Spring","Spring Framework","Spring Framework Description"),
+			new Topic("Java","Core Java","Core Java Description"),
+			new Topic("Javascript","Javascript Framework","Javascript Framework Description")
+			));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0;i<topics.size();i++) {
+			Topic t = topics.get(i);
+			if((id).equals(t.getId())) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+		
+	}
+}
